4aa331968efd229eb66fe2c1b6421297df3ecb43
InkoHX <me@inkohx.dev>
2022-11-17 00:25:33 +0900

    Update boostrap.mjs

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -1,15 +1,10 @@
 #!/usr/bin/env zx
 
 import 'zx/globals'
-import { spinner } from 'zx/experimental'
 
-spinner('Creating symbolic links...', () =>
-  Promise.all([
-    $`./zellij/create-symbolic.mjs`,
-    $`./vim/create-symbolic.mjs`,
-    $`./starship/create-symbolic.mjs`,
-    $`./gh/create-symbolic.mjs`,
-    $`./fish/create-symbolic.mjs`,
-    $`./alacritty/create-symbolic.mjs`,
-  ])
-)
+await $`./zellij/create-symbolic.mjs`
+await $`./vim/create-symbolic.mjs`
+await $`./starship/create-symbolic.mjs`
+await $`./gh/create-symbolic.mjs`
+await $`./fish/create-symbolic.mjs`
+await $`./alacritty/create-symbolic.mjs`
